0f71355a0af3829e99382fad785b007b83e519ac
16thAnU <114025131+16thAnU@users.noreply.github.com>
2023-05-31 12:07:07 +0100

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,33 @@
+// Your web app's Firebase configuration
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+    apiKey: "AIzaSyCP0P7h7to71wkKdbPrFPeKUGYtz_4lAD4",
+    authDomain: "lets-chat-web-app-1-te.firebaseapp.com",
+    projectId: "lets-chat-web-app-1-te",
+    storageBucket: "lets-chat-web-app-1-te.appspot.com",
+    messagingSenderId: "256917328386",
+    appId: "1:256917328386:web:f402d37252b51109704d1b",
+    measurementId: "G-PQQ6W6X1S1"
+  };
+  
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    /*
+    localStorage.setItem("user_name", user_name);
+    window.location = "chat_room.html";
+    */
+   localStorage.setItem("user_name", user_name);
+   window.location = "chat_room.html";
+}
+
+
+
